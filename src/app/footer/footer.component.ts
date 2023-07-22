f6c70015e81eadaee669c4178f65ec564b972f08
alamsalon <alambeautysalon@gmail.com>
2023-07-22 22:19:19 +0530

    new url changed

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  url: string;
+  constructor() { 
+  
+  }
 
   ngOnInit(): void {
+  
+    this.url = "https://instance.shivsofts.com/alam_salon/qr/index.php?branch_id=UzN4eE9JUEUzaUltOERvTTJKVVNGUT09";
   }
 
 }
